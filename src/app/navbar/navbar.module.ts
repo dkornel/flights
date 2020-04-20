c73ce57408cd194a11925026cca09f0e86e86425
Drabinski <kornel.drabinski@capgemini.com>
2020-04-20 12:05:00 +0200

    implement mat-button on navbar elements

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
 
 import { NavbarComponent } from "./navbar.component";
 
 @NgModule({
     imports: [
-        MatToolbarModule
+        MatToolbarModule,
+        MatButtonModule
     ],
     exports: [NavbarComponent],
     declarations: [NavbarComponent],
